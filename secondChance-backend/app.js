38e6bd0a1c3bfd924f58361d17de1d90d396919e
Kev-An <kevinanthony1849@gmail.com>
2025-06-21 23:01:09 -0400

    searchRoutes

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -29,7 +29,7 @@ app.use(express.json());
 const secondChanceItemsRoutes = require("./routes/secondChanceItemsRoutes")
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
+const searchRoutes = require("./routes/searchRoutes")
 
 
 const pinoHttp = require('pino-http');
@@ -42,7 +42,7 @@ app.use(pinoHttp({ logger }));
 app.use('/api/secondchance/items', secondChanceItemsRoutes);
 
 // Items API Task 2: add the secondChanceItemsRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/secondchance/search', searchRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
